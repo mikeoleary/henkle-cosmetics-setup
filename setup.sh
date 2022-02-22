62a57f8a62552e3d231fbdc3299d86c66e1af1a1
Michael O'Leary <38844924+mikeoleary@users.noreply.github.com>
2022-02-22 11:24:04 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 # Deployed Ubuntu 20.04 VM in Azure
 
+export COMPOSEVERSION="v2.2.3"
+
 sudo apt-get update
 sudo apt-get install docker.io -y
 sudo usermod -aG docker $USER         #add current user to docker group
@@ -9,8 +11,7 @@ newgrp docker                         #apply changes to group membership so regu
 docker pull wordpress:php8.1-apache
 
 #install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod
+sudo curl -L "https://github.com/docker/compose/releases/download/$COMPOSEVERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 mkdir henkle-cosmetics
